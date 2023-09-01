e21d384bd603667744f38a7445771fe4fd60bb3b
alfarojh <alfarous.jh@gmail.com>
2023-09-01 14:42:29 +0700

    fix query

--- Soal 3.sql
+++ Soal 3.sql
@@ -9,83 +9,111 @@ WHERE
 
 -- 3b
 SELECT 
-	"Peserta"."ID_Peserta" as "ID Peserta", 
-	"Peserta"."Nama" as "Nama Peserta", 
-	AVG("Nilai Tugas"."Nilai") as "Nilai Rata-rata"
+	"Peserta"."ID_Peserta" AS "ID Peserta", 
+	"Peserta"."Nama" AS "Nama Peserta", 
+	COALESCE(AVG("Nilai Tugas"."Nilai"), 0) AS "Nilai Rata-rata"
 FROM 
-	public."Nilai Tugas" JOIN public."Peserta" ON "Nilai Tugas"."ID_Peserta" = "Peserta"."ID_Peserta"
+	public."Nilai Tugas" RIGHT JOIN public."Peserta" ON "Nilai Tugas"."ID_Peserta" = "Peserta"."ID_Peserta"
 GROUP BY 
 	"Peserta"."ID_Peserta"
 ORDER BY 
 	"Peserta"."ID_Peserta" ASC
 
 -- 3c
-WITH AvgValues AS (
-    SELECT
-        "ID_Peserta",
-        AVG("Nilai") AS "Avg_Nilai"
-    FROM
-        public."Nilai Tugas"
-    GROUP BY
-        "ID_Peserta"
+WITH RentangNilai AS (
+    SELECT '0-20' AS "Rentang Nilai"
+    UNION
+    SELECT '20-40'
+    UNION
+    SELECT '40-60'
+    UNION
+    SELECT '60-80'
+    UNION
+    SELECT '80-100'
+	ORDER BY "Rentang Nilai"
+),
+AvgValues AS (
+    SELECT 
+        "Peserta"."ID_Peserta" AS "ID Peserta", 
+        AVG("Nilai Tugas"."Nilai") AS "Nilai Rata-rata"
+    FROM 
+        public."Nilai Tugas" RIGHT JOIN public."Peserta" ON "Nilai Tugas"."ID_Peserta" = "Peserta"."ID_Peserta"
+    GROUP BY 
+        "Peserta"."ID_Peserta"
 )
 SELECT
-    CASE
-        WHEN "Avg_Nilai" >= 0 AND "Avg_Nilai" < 20 THEN '0-19'
-        WHEN "Avg_Nilai" >= 20 AND "Avg_Nilai" < 40 THEN '20-39'
-        WHEN "Avg_Nilai" >= 40 AND "Avg_Nilai" < 60 THEN '40-59'
-        WHEN "Avg_Nilai" >= 60 AND "Avg_Nilai" <= 80 THEN '60-79'
-        WHEN "Avg_Nilai" >= 80 AND "Avg_Nilai" <= 100 THEN '80-100'
-        ELSE 'Tidak Valid'
-    END AS "Rentang Nilai",
-    COUNT(*) AS "Jumlah Peserta"
+    RentangNilai."Rentang Nilai",
+    COALESCE(COUNT(AvgValues."ID Peserta"), 0) AS "Jumlah Peserta"
 FROM
-    AvgValues
+    RentangNilai
+LEFT JOIN
+    AvgValues ON (
+        (RentangNilai."Rentang Nilai" = '0-20' AND "Nilai Rata-rata" BETWEEN 0 AND 20) OR
+        (RentangNilai."Rentang Nilai" = '20-40' AND "Nilai Rata-rata" BETWEEN 20 AND 40) OR
+        (RentangNilai."Rentang Nilai" = '40-60' AND "Nilai Rata-rata" BETWEEN 40 AND 60) OR
+        (RentangNilai."Rentang Nilai" = '60-80' AND "Nilai Rata-rata" BETWEEN 60 AND 80) OR
+        (RentangNilai."Rentang Nilai" = '80-100' AND "Nilai Rata-rata" BETWEEN 80 AND 100)
+    )
 GROUP BY
-    "Rentang Nilai"
+    RentangNilai."Rentang Nilai"
 ORDER BY
-    "Rentang Nilai";
+    RentangNilai."Rentang Nilai";
+
 	
 -- 3d
-WITH AvgValues AS (
-    SELECT
-        "ID_Peserta",
-        AVG("Nilai") AS "Avg_Nilai"
-    FROM
-        public."Nilai Tugas"
-    GROUP BY
-        "ID_Peserta"
+WITH RentangNilai AS (
+    SELECT '0-20' AS "Rentang Nilai"
+    UNION
+    SELECT '20-40'
+    UNION
+    SELECT '40-60'
+    UNION
+    SELECT '60-80'
+    UNION
+    SELECT '80-100'
+	ORDER BY "Rentang Nilai"
+),
+AvgValues AS (
+    SELECT 
+        "Peserta"."ID_Peserta" AS "ID Peserta", 
+		"Peserta"."Nama" AS "Nama Peserta",
+        AVG("Nilai Tugas"."Nilai") AS "Nilai Rata-rata"
+    FROM 
+        public."Nilai Tugas" RIGHT JOIN public."Peserta" ON "Nilai Tugas"."ID_Peserta" = "Peserta"."ID_Peserta"
+    GROUP BY 
+        "Peserta"."ID_Peserta"
 )
 SELECT
-    CASE
-        WHEN "Avg_Nilai" >= 0 AND "Avg_Nilai" < 20 THEN '0-19'
-        WHEN "Avg_Nilai" >= 20 AND "Avg_Nilai" < 40 THEN '20-39'
-        WHEN "Avg_Nilai" >= 40 AND "Avg_Nilai" < 60 THEN '40-59'
-        WHEN "Avg_Nilai" >= 60 AND "Avg_Nilai" <= 80 THEN '60-79'
-        WHEN "Avg_Nilai" >= 80 AND "Avg_Nilai" <= 100 THEN '80-100'
-        ELSE 'Tidak Valid'
-    END AS "Rentang Nilai",
-    STRING_AGG("Peserta"."Nama", ', ') AS "Nama Peserta"
+    RentangNilai."Rentang Nilai",
+    COALESCE(STRING_AGG(DISTINCT "Nama Peserta", ', '), 'Tidak Ada Peserta') AS "Nama Peserta"
 FROM
-    AvgValues JOIN public."Peserta" ON AvgValues."ID_Peserta" = "Peserta"."ID_Peserta"
-GROUP BY 
-    "Rentang Nilai"
+    RentangNilai
+LEFT JOIN
+    AvgValues ON (
+        (RentangNilai."Rentang Nilai" = '0-20' AND "Nilai Rata-rata" BETWEEN 0 AND 20) OR
+        (RentangNilai."Rentang Nilai" = '20-40' AND "Nilai Rata-rata" BETWEEN 20 AND 40) OR
+        (RentangNilai."Rentang Nilai" = '40-60' AND "Nilai Rata-rata" BETWEEN 40 AND 60) OR
+        (RentangNilai."Rentang Nilai" = '60-80' AND "Nilai Rata-rata" BETWEEN 60 AND 80) OR
+        (RentangNilai."Rentang Nilai" = '80-100' AND "Nilai Rata-rata" BETWEEN 80 AND 100)
+    )
+GROUP BY
+    RentangNilai."Rentang Nilai"
 ORDER BY
-    "Rentang Nilai";
+    RentangNilai."Rentang Nilai";
 
 -- 3e
-SELECT
-    CASE
-        WHEN "Nilai Tugas"."Tanggal" = '2023-01-01' THEN '1 Agustus 2023'
-        WHEN "Nilai Tugas"."Tanggal" = '2023-01-02' THEN '2 Agustus 2023'
-        WHEN "Nilai Tugas"."Tanggal" = '2023-01-03' THEN '3 Agustus 2023'
-        WHEN "Nilai Tugas"."Tanggal" = '2023-01-04' THEN '4 Agustus 2023'
-        ELSE 'Tidak Valid'
-    END AS "Rentang Tanggal",
-    COUNT (*) AS "Jumlah Penilaian"
-FROM
-    public."Nilai Tugas"
-GROUP BY 
-    "Rentang Tanggal"
+WITH RangeDate AS (
+    SELECT 
+		TO_CHAR(generated_date, 'DD-MM-YYYY') AS "Tanggal"
+	FROM 
+		generate_series('2023-01-01'::date, '2023-01-31'::date, '1 day') AS generated_date
+)
+
+SELECT 
+	RangeDate."Tanggal", COUNT("Nilai Tugas"."ID_Peserta") AS "Jumlah Penilaian"
+FROM 
+	RangeDate LEFT JOIN public."Nilai Tugas" ON RangeDate."Tanggal"::date = "Nilai Tugas"."Tanggal"
+GROUP BY
+	RangeDate."Tanggal"
 ORDER BY
-    "Rentang Tanggal";
+	RangeDate."Tanggal"
